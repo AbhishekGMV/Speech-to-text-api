a085b75355ab8f5469202dded222b0108cc4f75f
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-04-15 13:57:34 +0530

    API for audio file transcription

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import speech_recognition as sr
+from os import path
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def convertAudioFiletoText():
+    if request.method == 'GET':
+        return "<h3>Make a post request with an audio file for transcription</h3>"
+
+    AUDIO_FILE = request.files['audio']
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.record(source)
+        response = recognizer.recognize_google(audio, language='en-IN')
+        json_response = jsonify({"responseText": response})
+    return json_response
+app.run()    
