470aa68b4bf6f42a56310b222ceb016922065507
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-04-15 16:08:08 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def index():
     return "<h3>Make a post request with an audio file for transcription</h3>"
 
-# @app.route('/transcribeAudio', methods=['POST'])
-# def convertAudioFiletoText():
-#     AUDIO_FILE = request.files['audio']
-#     recognizer = sr.Recognizer()
-#     with sr.AudioFile(AUDIO_FILE) as source:
-#         recognizer.adjust_for_ambient_noise(source)
-#         audio = recognizer.record(source)
-#         response = recognizer.recognize_google(audio, language='en-IN')
-#         json_response = jsonify({"responseText": response})
-#     return json_response
+@app.route('/transcribeAudio', methods=['POST'])
+def convertAudioFiletoText():
+    AUDIO_FILE = request.files['audio']
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.record(source)
+        response = recognizer.recognize_google(audio, language='en-IN')
+        json_response = jsonify({"responseText": response})
+    return json_response
 app.run()    
